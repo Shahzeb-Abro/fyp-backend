333303a76c00d1d938ba7adab87cab994006e643
AbroShahzeb <khizr772@gmail.com>
2025-04-24 19:04:09 +0500

    Connected database and implemented global error handling

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./utils/db.js";
+dotenv.config();
+import "colors";
 const app = express();
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import globalErrorHandler from "./controllers/error.controller.js";
+
+connectDB();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World! hehe");
 });
 
-app.listen(3009, () => {
-  console.log("Server is running on port 3009 test again");
+app.use(globalErrorHandler);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
