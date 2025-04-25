29130225d02cad80655a9e8970ac537da34405f8
AbroShahzeb <khizr772@gmail.com>
2025-04-26 02:01:20 +0500

    Increased request body size limit

--- routes/detection.route.js
+++ routes/detection.route.js
@@ -14,7 +14,10 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 10 * 1024 * 1024 },
+});
 
 router.post("/", authorize, upload.single("file"), detectUlcer);
 
